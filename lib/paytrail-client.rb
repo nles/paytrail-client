eca50815bef80e1b43bb05c4e8dbf7f9ad66f9e3
Joakim Antman <antmanj@gmail.com>
2016-03-06 20:39:43 +0200

    Init continues

---
+++ lib/paytrail-client.rb
@@ -0,0 +1,1 @@
+require 'paytrail_client'
---
+++ spec/paytrail_client/client_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe PaytrailClient do
+  it 'has a version number' do
+    expect(PaytrailClient::VERSION).not_to be nil
+  end
+end
